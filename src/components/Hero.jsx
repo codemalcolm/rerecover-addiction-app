16a67d04a7131621a40cd3292c7d73f481ea48ff
Vojta Cvrcheck <vojta.cvrcek1@gmail.com>
2024-03-07 20:43:22 +0100

    Create-habit-ui-layout

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import CreateHabit from './Navigation/CreateHabit'
 
 const Hero = () => {
   return (
-    <div className="border border-red-900 mx-6 mt-4 flex justify-center">
+    <div className="mx-6 mt-4 flex justify-center">
         <CreateHabit/>
     </div>
   )
--- src/components/Navigation/CreateHabit.jsx
+++ src/components/Navigation/CreateHabit.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
+import React from "react";
 
 const CreateHabit = () => {
-  return (
-    <div className="mt-6 border border-blue-900 flex-row items-center rounded-xl p-8">
-        <div className="flex-row justify-center">
-            <h2 className="font-bold text-xl mt-2 mb-4 text-center">Create your habit</h2>
-            <input type="text" className="border border-black rounded text-center"/>
-        </div>
-    </div>
-  )
-}
+	return (
+		<div className="mt-16 border border-slate-400 flex-row items-center w-80 rounded-xl px-8">
+			<div className="relative flex justify-center items-center">
+				<div className="border border-black text-center w-28 h-28 rounded-full leading-24 absolute bg-white flex items-center p-2">
+					Image Picker
+				</div>
+			</div>
+			<div className="w-full m-auto mt-16 text-center">
+				<h2 className="font-bold text-xl">Create your habit</h2>
+				<input
+					type="text"
+					className="border-2 border-black rounded px-2 mt-2"
+					placeholder="Habit Name"
+				/>
+				<input
+					type="text"
+					className="border-2 border-black rounded px-2 py-4 mt-2"
+					placeholder="Habit Description"
+				/>
+				<button className="border-2 border-black mt-2 mb-4 px-8 py-2 rounded-full font-semibold">
+					ADD HABIT
+				</button>
+			</div>
+		</div>
+	);
+};
 
-export default CreateHabit
+export default CreateHabit;
