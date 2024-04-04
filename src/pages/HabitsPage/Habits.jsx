8c4737e532963c0b9185d603170e9baa78195889
Vojtěch Cvrček <vojta.cvrcek1@gmail.com>
2024-04-04 13:23:32 +0200

    emoji picker modal

--- src/pages/HabitsPage/Habits.jsx
+++ src/pages/HabitsPage/Habits.jsx
@@ -15,11 +15,13 @@ import {
   createMultiStyleConfigHelpers,
   defineStyle,
 } from '@chakra-ui/styled-system'
-
+import EmojiPicker from 'emoji-picker-react';
+import { useState } from "react";
 
 
 const Habits = () => {
 	const { isOpen, onOpen, onClose } = useDisclosure();
+	const [ isPickerOpen, setIsPickerOpen] = useState(false)
 	return (
 		<>
 			<Flex justifyContent={"end"} mt={5}>
@@ -40,19 +42,29 @@ const Habits = () => {
 						/>
 					</Button>
 				</Tooltip>
+				<Button
+				
+				>Open emoji picker</Button>
+				
+
 			</Flex>
 
 			<Modal isOpen={isOpen} onClose={onClose} size={"md"}>
 				<ModalOverlay />
 				<ModalContent>
                 <Flex position={"relative"} justifyContent={"center"} alignItems={"center"}>
-                    <Flex border={"2px solid black"} width={"112px"} height={"112px"}
+                    <Flex border={"2px solid black"} width={"112px"} height={"112px"} onClick={()=> setIsPickerOpen(!isPickerOpen)}
                         rounded={"full"} justifyContent={"center"} alignItems={"center"} position={"absolute"} bg={"white"} cursor={"pointer"}>
                             <SlPicture style={{
                                 width:"50px",
                                 height:"50px"
                             }}/>
+
                     </Flex>
+					<Flex position={"fixed"} top={"18%"} left={"7%"} zIndex={9999}>
+						<EmojiPicker open={isPickerOpen} onEmojiClick={(e)=>console.log(e)} position={"absolute"}/>
+					</Flex>
+
                 </Flex>
 					
 					<ModalHeader>Create a habit</ModalHeader>
@@ -68,7 +80,6 @@ const Habits = () => {
 						<Button colorScheme="blue" mr={3} onClick={onClose}>
 							Close
 						</Button>
-						<Button variant="ghost">Secondary Action</Button>
 					</ModalFooter>
 				</ModalContent>
 			</Modal>
