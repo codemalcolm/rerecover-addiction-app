b7329b970b94d780a86ed9b9bc3ce92077fe32f1
Vojta Cvrcheck <vojta.cvrcek1@gmail.com>
2024-04-11 21:35:53 +0200

    Habit calendar db dates mocking, state updating

--- src/components/HabitCalendar.jsx
+++ src/components/HabitCalendar.jsx
@@ -1,60 +1,87 @@
-import { useEffect, useState } from 'react';
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
-import Tick from './icons/Tick';
-import Cross from './icons/Cross';
-
+import { useEffect, useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import Tick from "./icons/Tick";
+import Cross from "./icons/Cross";
 
 const HabitCalendar = () => {
-    const [value, setValue] = useState(null);
-    const [clickedDates, setClickedDates] = useState({});
-    const [dateLogs, setDateLogs] = useState({
-      logDate:"",
-      state:""
-    })
+	const [value, setValue] = useState(null);
+	const [dateLogs, setDateLogs] = useState([]);
 
+	// Dates from database
+	const randomDateLogs = [
+		{ logDate: "2024-04-10", state: "done" },
+		{ logDate: "2024-04-17", state: "not-done" },
+		{ logDate: "2024-04-26", state: "done" },
+		{ logDate: "2024-04-12", state: "not-done" },
+		{ logDate: "2024-04-25", state: "done" },
+		{ logDate: "2024-04-29", state: "done" },
+		{ logDate: "2024-04-09", state: "not-done" },
+		{ logDate: "2024-04-05", state: "done" },
+	];
 
+	// Initialize dateLogs state with dates from randomDateLogs
+	useEffect(() => {
+		setDateLogs(randomDateLogs);
+	}, []);
 
-    const handleDateClick = (date) => {
-        const formattedDate = date.toISOString().split('T')[0]
-        console.log(formattedDate + "here")
-        if (formattedDate) {
-          if (clickedDates[formattedDate]) {
-            setDateLogs(prevLogs => ({ ...prevLogs, logDate: formattedDate, state: "done" }));
-          } else if (clickedDates.hasOwnProperty(formattedDate) && !clickedDates[formattedDate]) {
-            setDateLogs(prevLogs => ({ ...prevLogs, logDate: formattedDate, state: "not-done" }));
-          } else {
-            setDateLogs(prevLogs => ({ ...prevLogs, logDate: formattedDate, state: "" }));
-          }
-        }
+	const formatDate = (date) => {
+		const year = date.getFullYear();
+		const month = date.getMonth() + 1;
+		const day = date.getDate();
+		return `${year}-${month < 10 ? "0" : ""}${month}-${
+			day < 10 ? "0" : ""
+		}${day}`;
+	};
 
-        setClickedDates(prevDates => ({
-          ...prevDates,
-          [formattedDate] : !prevDates[formattedDate]
-        }));
-        console.log(dateLogs)
-    }
+	const handleDateClick = (date) => {
+		const formattedDate = formatDate(date);
+		const logEntryIndex = dateLogs.findIndex(
+			(log) => log.logDate === formattedDate
+		);
 
-    const tileIcon = ({ date, view }) => {
-        const formattedDate = date.toISOString().split('T')[0];
-        if (view === 'month' && clickedDates[formattedDate]) {
+		// Update dateLogs state
+		if (logEntryIndex !== -1) {
+			// If the date already exists in dateLogs, toggle its state
+			const updatedDateLogs = [...dateLogs];
+			updatedDateLogs[logEntryIndex].state =
+				dateLogs[logEntryIndex].state === "done" ? "not-done" : "done";
+			setDateLogs(updatedDateLogs);
+		} else {
+			// If the clicked date is not in dateLogs, add it with the new state
+			setDateLogs((prevLogs) => [
+				...prevLogs,
+				{ logDate: formattedDate, state: "done" },
+			]);
+		}
+		console.log(dateLogs);
+	};
 
-            return <div style={{position:"relative"}}><Tick/></div>
+	const tileIcon = ({ date, view }) => {
+		const formattedDate = formatDate(date);
+		const logEntry = dateLogs.find((log) => log.logDate === formattedDate);
 
-          } else if (view === "month" && clickedDates.hasOwnProperty(formattedDate) && !clickedDates[formattedDate]){
+		if (view === "month") {
+			if (logEntry) {
+				return (
+					<div style={{ position: "relative" }}>
+						{logEntry.state === "done" ? <Tick /> : <Cross />}
+					</div>
+				);
+			} else {
+				return null;
+			}
+		}
+	};
 
-            return <div style={{position:"relative"}}><Cross/></div>
+	return (
+		<Calendar
+			value={value}
+			locale="en-US"
+			onClickDay={handleDateClick}
+			tileContent={(props) => tileIcon(props)}
+		/>
+	);
+};
 
-          } else if (view === 'month' && !clickedDates.hasOwnProperty(formattedDate)){
-            return null;
-          }
-    }
-
-  return (
-    <>
-        <Calendar value={value} onClickDay={(e)=> handleDateClick(e)} tileContent={tileIcon}/>
-    </>
-  )
-}
-
-export default HabitCalendar
+export default HabitCalendar;
