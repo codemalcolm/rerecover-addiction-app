b9bdfc6f00a1341cce3b3b2cebfa35a5c7575ec9
Vojta Cvrcheck <vojta.cvrcek1@gmail.com>
2024-04-17 22:39:27 +0200

    authpage mobile version

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -4,33 +4,33 @@ import { Link } from "react-router-dom";
 
 const AuthPage = () => {
 	return (
-		<Flex justifyContent={"center"} alignItems={"center"} flexDirection={"column"} my={40}>
+		<Flex flexDirection={"row"} justifyContent={"center"} alignItems={"center"} my={"62px"}
+		height={"full"}>
 			<Flex
 				gap={5}
 				backgroundColor={"#f9f9f9"}
-				minW={"600px"}
+				minW={{base:"430px", md:"600px"}}
 				justifyContent={"center"}
 				flexDirection={"column"}
 				alignItems={"center"}
 				py={40}
+				borderRadius={"6px"}
 			>
-			<Link to='/'>
-				<Text
-					textAlign={"center"}
-					fontSize={36}
-					fontFamily={"Eczar"}
-					fontWeight={700}
-					_hover={{color:"#545454"}}
-					transition={"0.1s ease-in-out"}
-				>
-					ReRecover
-				</Text>
-			</Link>
-			<AuthForm/>
+				<Link to="/">
+					<Text
+						textAlign={"center"}
+						fontSize={36}
+						fontFamily={"Eczar"}
+						fontWeight={700}
+						_hover={{ color: "#545454" }}
+						transition={"0.1s ease-in-out"}
+					>
+						ReRecover
+					</Text>
+				</Link>
+				<AuthForm />
 			</Flex>
-			
 		</Flex>
-
 	);
 };
 
--- src/pages/AuthPage/Login.jsx
+++ src/pages/AuthPage/Login.jsx
@@ -1,6 +1,7 @@
-import { Button, Flex, Input } from "@chakra-ui/react";
+import { Button, Flex, Input, InputGroup, InputRightElement } from "@chakra-ui/react";
 import useLogin from "../../hooks/useLogin";
 import { useState } from "react";
+import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 
 const Login = () => {
 	const [inputs, setInputs] = useState({
@@ -8,6 +9,7 @@ const Login = () => {
 		password: "",
 	});
 	const {loading, error, login} = useLogin()
+	const [showPassword, setShowPassword] = useState(false);
 	return (
 		<Flex flexDirection={"column"} gap={4} w={"320px"}>
 			<Input
@@ -17,12 +19,24 @@ const Login = () => {
 				size={"md"}
 				onChange={(e) => setInputs({ ...inputs, email: e.target.value })}
 			/>
-			<Input
-				placeholder="Password"
-				value={inputs.password}
-				size={"md"}
-				onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
-			/>
+			<InputGroup>
+				<Input
+					placeholder="Password"
+					value={inputs.password}
+					size={"md"}
+					onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
+					type={showPassword ? "text" : "password"}
+				/>
+				<InputRightElement h="full">
+					<Button
+						variant={"ghost"}
+						size={"sm"}
+						onClick={() => setShowPassword(!showPassword)}
+					>
+						{showPassword ? <ViewIcon /> : <ViewOffIcon />}
+					</Button>
+				</InputRightElement>
+			</InputGroup>
             <Button w={"100%"}
 			isLoading={loading} 
 			onClick={() => login(inputs)}
