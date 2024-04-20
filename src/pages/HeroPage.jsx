c6b3a714b71f011cd701a5b7e1be494a0173acfb
Vojta Cvrcheck <vojta.cvrcek1@gmail.com>
2024-04-20 15:18:42 +0200

    how to guide start

--- src/pages/HeroPage.jsx
+++ src/pages/HeroPage.jsx
@@ -1,12 +1,73 @@
-import { Flex } from "@chakra-ui/react"
-import HabitCalendar from "../components/HabitCalendar"
+import { Flex, Text, Box, Image } from "@chakra-ui/react";
+import HabitCalendar from "../components/HabitCalendar";
 
+import howToHabit from "../images/how-to-create-habit.png";
 const HeroPage = () => {
-  return (
-    <Flex w={"full"}>
-    HeroPage
-    </Flex>
-  )
-}
+	return (
+		<Flex justifyContent={"center"}>
+			<Flex mt={12} flexDirection={"column"}>
+				<Text fontSize={30} fontFamily={"Kanit"} textAlign={"center"} width={"full"}>
+					How to use
+					<span
+						style={{
+							fontSize: 42,
+							fontFamily: "Eczar",
+							fontWeight: 700,
+							marginLeft: 9,
+              display:"flex",
+              justifyContent:"center"
+						}}
+					>
+					ReRecover
+					</span>
+				</Text>
 
-export default HeroPage
+        {/* How to Create Habit */}
+        <Flex flexDirection={"column"}>
+          <Flex justifyContent={{base:"center", md:"start"}}>
+            <Text
+              fontSize={28}
+              fontFamily={"Kanit"}
+              mt={16}
+              bg={"#ff914d"}
+              p={1}
+              borderRadius={8}
+              width={"240px"}
+              color={"white"}
+              textAlign={"center"}
+            >
+              Creating a habit
+            </Text>
+          </Flex>
+          <Box>
+            <Image src={howToHabit} />
+          </Box>
+        </Flex>
+
+        {/* How to Edit Habit */}
+        <Flex flexDirection={"column"}>
+          <Flex justifyContent={{base:"center", md:"start"}}>
+            <Text
+              fontSize={28}
+              fontFamily={"Kanit"}
+              mt={16}
+              bg={"#FFBC4D"}
+              p={1}
+              borderRadius={8}
+              width={"240px"}
+              color={"white"}
+              textAlign={"center"}
+            >
+              Editting a habit
+            </Text>
+          </Flex>
+          <Box>
+            <Image src={howToHabit} />
+          </Box>
+        </Flex>
+			</Flex>
+		</Flex>
+	);
+};
+
+export default HeroPage;
